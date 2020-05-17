b93f3b7517e8f06ca26d220f319c80e544d58649
choirulihwan <aristhu_oracle@yahoo.com>
2020-05-18 06:36:13 +0700

    scraping haji with flask and bootstrap => done

---
+++ flask_haji.py
@@ -0,0 +1,13 @@
+from flask import Flask as Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return "Bismillah"
+    return render_template('index_haji.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
