2ba32b2a00d746bb354110c7af5f718979d1a6d8
choirulihwan <aristhu_oracle@yahoo.com>
2020-05-19 19:22:20 +0700

    scraping dengan requests with cookies

--- test_scraping_haji.py
+++ test_scraping_haji.py
@@ -1,28 +1,45 @@
-import urllib2
+# import urllib2
 # handler = urllib2.HTTPHandler(debuglevel = 1)
+import requests
 from bs4 import BeautifulSoup
+
+
+# def scrap_haji_27(no_porsi):
+#     url = 'https://haji.kemenag.go.id/v4/estimasi-keberangkatan'
+#     ls_cookie = 'nomor_porsi=' + no_porsi
+#
+#     req = urllib2.Request(url)
+#     req.add_header('Cookie', (ls_cookie))
+#
+#     # opener = urllib2.build_opener(handler)
+#     # urllib2.install_opener(opener)
+#
+#     resp = urllib2.urlopen(req)
+#     # print(resp.read())
+#
+#     soup = BeautifulSoup(resp, 'html.parser')
+#     data = soup.find_all('td')
+#     # print(data)
+#     # print('Nomor porsi: {}'.format(data[8].get_text()))
+#     data_haji = {}
+#     data_haji['nomor_porsi'] = data[8].get_text()
+#     # print(data_haji)
+#     return data_haji
+
 
 
 def scrap_haji(no_porsi):
     url = 'https://haji.kemenag.go.id/v4/estimasi-keberangkatan'
-    ls_cookie = 'nomor_porsi=' + no_porsi
+    cookies = dict(nomor_porsi=no_porsi)
 
-    req = urllib2.Request(url)
-    req.add_header('Cookie', (ls_cookie))
+    resp = requests.get(url, cookies=cookies)
+    print(resp.text)
 
-    # opener = urllib2.build_opener(handler)
-    # urllib2.install_opener(opener)
-
-    resp = urllib2.urlopen(req)
-    # print(resp.read())
-
-    soup = BeautifulSoup(resp, 'html.parser')
+    soup = BeautifulSoup(resp.text, 'html.parser')
     data = soup.find_all('td')
-    # print(data)
-    # print('Nomor porsi: {}'.format(data[8].get_text()))
     data_haji = {}
     data_haji['nomor_porsi'] = data[8].get_text()
-    # print(data_haji)
+
     return data_haji
 
 print(scrap_haji('1200087878'))
